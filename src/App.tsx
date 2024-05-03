aae13f19c328acc26a66f95b66a93470679655ce
joseph1261997 <lccgpp97h12f258q@studium.unict.it>
2024-05-03 21:57:41 +0200

    use material ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import TodoList from './components/TodoList';
+import Typography from '@mui/material/Typography';
+import { Box } from '@mui/material';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>Todo List</h1>
+    <Box>
+      <Typography variant="h2" align='center' gutterBottom>Todo List</Typography>
       <TodoList />
-    </div>
+    </Box>
   );
 };
 
